b8f5f718b5e5a6d0bf8d4a3e85f83f5f2201e759
Archi Danva <archidanva@gmail.com>
2026-05-11 00:39:00 +0530

    Full Functional

--- server/middleware/protect.js
+++ server/middleware/protect.js
@@ -1,14 +1,17 @@
+import jwt from "jsonwebtoken";
 import { promisify } from "util";
+
 const verifyAsync = promisify(jwt.verify);
 
 const protect = async (req, res, next) => {
   try {
     const token = req.cookies.token;
-    if (!token) return res.status(401).json({...});
+    if (!token) return res.status(401).json({ success: false, message: "Not authorized." });
+
     req.user = await verifyAsync(token, process.env.JWT_SECRET);
     next();
   } catch {
-    return res.status(401).json({...});
+    return res.status(401).json({ success: false, message: "Invalid token." });
   }
 };
 
